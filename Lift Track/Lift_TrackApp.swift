eceb8ab1020fc496da1fb3508fb031252b69a569
linglejack06 <captainjack0818@gmail.com>
2022-11-03 20:39:20 -0400

    Initial Commit

---
+++ Lift Track/Lift_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lift_TrackApp.swift
+//  Lift Track
+//
+//  Created by Jack Lingle on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Lift_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
